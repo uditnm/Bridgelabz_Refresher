69f504d1bb1bcd712ef363844d853378b101636d
uditnm <udit20001@gmail.com>
2023-02-18 23:05:15 +0530

    [Udit] Added program for Factors

---
+++ ProgramsDay2/Factors.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Factors
+    {
+        public void factor(int n)
+        {
+            while(n%2==0)
+            {
+                Console.WriteLine(2);
+                n /= 2;
+            }
+
+            for(int i=3; i*i<=n; i += 2)
+            {
+                while(n%i==0)
+                {
+                    Console.WriteLine(i);
+                    n /= i;
+                }
+            }
+
+            if (n > 2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
