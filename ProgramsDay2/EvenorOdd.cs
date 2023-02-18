c611bd9bfc269a3b928aa75ecc1074bfb441e80c
uditnm <udit20001@gmail.com>
2023-02-18 23:04:53 +0530

    [Udit] Added program for even or odd number

---
+++ ProgramsDay2/EvenorOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class EvenorOdd
+    {
+        public void check(int n)
+        {
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
