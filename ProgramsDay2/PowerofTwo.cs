29faa9c2734a2b3b4d7c85378d0b2ed5270004cc
uditnm <udit20001@gmail.com>
2023-02-18 23:10:36 +0530

    [Udit] Added program for Powers of two

---
+++ ProgramsDay2/PowerofTwo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class PowerofTwo
+    {
+        public void power(int n)
+        {
+            for(int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
