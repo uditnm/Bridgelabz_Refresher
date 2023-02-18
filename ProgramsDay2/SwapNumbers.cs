65e38da2dbcc16fffbe0d53f4942833383dddb01
uditnm <udit20001@gmail.com>
2023-02-18 23:17:27 +0530

    [Udit] Added program for Swapping numbers

---
+++ ProgramsDay2/SwapNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class SwapNumbers
+    {
+        public void swap(int num1,int num2)
+        {
+            Console.WriteLine("The original numbers are: {0} {1}", num1, num2);
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("The swapped numbers are: {0} {1}", num1, num2);
+        }
+    }
+}
