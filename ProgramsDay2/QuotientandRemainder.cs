4e6c756a055672dc2d2338327174bbd6fda2bf6c
uditnm <udit20001@gmail.com>
2023-02-18 23:16:51 +0530

    [Udit] Added program for Quotient and Remainder

---
+++ ProgramsDay2/QuotientandRemainder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class QuotientandRemainder
+    {
+        public void division(int a, int b)
+        {
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine("The quotient is: " + quotient);
+            Console.WriteLine("The remainder is: " + remainder);
+        }
+    }
+}
