849e73800a5138e5ed1d89759a82991ee9270204
uditnm <udit20001@gmail.com>
2023-02-18 23:05:49 +0530

    [Udit] Added program for Head or Tails

---
+++ ProgramsDay2/FlipCoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class FlipCoin
+    {
+        public void flip(int n)
+        {
+            float heads = 0, tails = 0;
+            double flip = 0;
+            Random r = new Random();
+            for(int i=0;i<n; i++)
+            {
+                flip = r.NextDouble();
+                if (flip < 0.5)
+                {
+                    tails++;
+                }
+                else
+                {
+                    heads++;
+                }
+
+            }
+
+            Console.WriteLine("Percentage of Heads: " + heads / n);
+            Console.WriteLine("Percentage of Tails: " + tails / n);
+        }
+    }
+}
