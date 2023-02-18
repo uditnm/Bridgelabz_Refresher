d8b63b2c153045ee5d79e56518eeaefa91e616cc
uditnm <udit20001@gmail.com>
2023-02-18 23:09:15 +0530

    [Udit] Added program for Harmonic number

---
+++ ProgramsDay2/HarmonicNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class HarmonicNumber
+    {
+        public void harmonic(int n)
+        {
+            float harmonicnum=0;
+            for(float i=1;i<=n;i++)
+            {
+                harmonicnum += 1/i;
+            }
+            Console.WriteLine("The nth harmonic number is: " + harmonicnum);
+        }
+    }
+}
