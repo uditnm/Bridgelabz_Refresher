503b48958e7a5fa855dbd15078a652b3e84f3c9c
uditnm <udit20001@gmail.com>
2023-02-18 23:10:13 +0530

    [Udit] Added program for Leap year

---
+++ ProgramsDay2/LeapYear.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class LeapYear
+    {
+        public void isLeap(int year)
+        {
+            if((year%400==0||year%100!=0) && year % 4 == 0)
+            {
+                Console.WriteLine("It is a Leap year");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Leap year");
+            }
+        }
+    }
+}
