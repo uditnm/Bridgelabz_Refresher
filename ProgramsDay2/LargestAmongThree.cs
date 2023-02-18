b10f855c6ba22a566bdfd585f72335b1915c0e5f
uditnm <udit20001@gmail.com>
2023-02-18 23:09:49 +0530

    [Udit] Added program for Largest among three numbers

---
+++ ProgramsDay2/LargestAmongThree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class LargestAmongThree
+    {
+        public int largest(int x, int y, int z)
+        {
+            int largestnum;
+            
+            if (x >= y && x >= z)
+            {
+                largestnum = x;
+            }
+            else if (y >= x && y >= z)
+            {
+               largestnum = y;
+            }
+            else 
+            {
+               largestnum = z;
+            }
+
+            return largestnum;
+        }
+        
+        
+    }
+}
