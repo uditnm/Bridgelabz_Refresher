5fe77f528063425e24fc90c7548ac21c8d8b7c8e
uditnm <udit20001@gmail.com>
2023-02-21 19:08:00 +0530

    [Udit] added program for Gambler

---
+++ LogicalPrograms/Gambler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Gambler
+    {
+        public void gamble(int stake, int goal, int times)
+        {
+            double outcome;
+            double win = 0, loss = 0;
+            Random r = new Random();
+            for(int i=0;i<times;i++)
+            {
+                while (stake != 0)
+                {
+                    outcome = r.NextDouble();
+                    if (outcome > 0.5)
+                    {
+                        stake++;
+                        if (stake == goal)
+                        {
+                            win++;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        stake--;
+                    }
+                }
+                if (stake == 0)
+                {
+                    loss++;
+                }
+            }
+
+            Console.WriteLine("Gambler won {0} times", win);
+            Console.WriteLine("Gambler lost {0} times", loss);
+            Console.WriteLine("Percentage of win: " + (win / times) * 100);
+            Console.WriteLine("Percentage of loss: " + (loss / times) * 100);
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = true;
+            while(t)
+            {
+                Console.WriteLine("1.Gambler \n2.Coupon Numbers \n3.Simulate Stopwatch Program " +
+                    "\n4.Tic Tac Toe Game \n5.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        Gambler gambler = new Gambler();
+                        Console.WriteLine("Enter the stake: ");
+                        int stake = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the goal: ");
+                        int goal = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the no. of times");
+                        int times = Convert.ToInt32(Console.ReadLine());
+                        gambler.gamble(stake, goal, times);
+                        break;
+                }
+            }
+        }
+    }
+}
