6a3f54b09868a082b35fc17dafb968ec96f040d9
uditnm <udit20001@gmail.com>
2023-02-21 14:42:12 +0530

    [Udit] added program for 2D Array

---
+++ FunctionalPrograms/Array2D.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    internal class Array2D
+    {
+        public void readintarray(int m, int n)
+        {
+
+            int[,] arr = new int[m,n];
+            Console.WriteLine("Enter the elements");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The array is: ");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(arr[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void readdoublearray(int m, int n)
+        {
+            double[,] arr = new double[m,n];
+
+
+            Console.WriteLine("Enter the elements");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i,j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The array is: ");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void readbooleanarray(int m, int n)
+        {
+            bool[,] arr = new bool[m,n];
+
+
+            Console.WriteLine("Enter the elements");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = Convert.ToBoolean(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The array is: ");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
---
+++ FunctionalPrograms/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FunctionalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = true;
+            while (t)
+            {
+                Console.WriteLine("1.2D Array \n2.Sum of three integers adds to zero \n3.Euclidean Distance " +
+                    "\n4.Quadratic roots \n5.WindChill \n6.Exit");
+                int ch = Convert.ToInt32(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+                        Array2D array2d = new Array2D();
+                        Console.WriteLine("Enter the rows and columns:");
+                        int m = Convert.ToInt32(Console.ReadLine());
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("1.Integer array \n2.Double array \n3.boolean array");
+                        int c = Convert.ToInt32(Console.ReadLine());
+                        switch (c)
+                        {
+                            case 1:
+                                array2d.readintarray(m, n);
+                                break;
+                            case 2:
+                                array2d.readdoublearray(m, n);
+                                break;
+                            case 3:
+                                array2d.readbooleanarray(m, n);
+                                break;
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
