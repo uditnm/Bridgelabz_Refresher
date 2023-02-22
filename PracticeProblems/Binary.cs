54864a1a9f6f59e749f24e85f0c469766743d13b
uditnm <udit20001@gmail.com>
2023-02-22 12:35:18 +0530

    [Udit] added program for swapping nibbles

---
+++ PracticeProblems/Binary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Binary
+    {
+        public void toBinary(int num)
+        {
+            string binary = Convert.ToString(num, 2);
+            Console.WriteLine(binary);
+
+            int right = num & 0b00001111;
+            right = right << 4;
+            int left = num & 0b11110000;
+            left = left >> 4;
+
+            int newnum = right | left;
+            Console.WriteLine("The number after swap is: " + newnum);
+
+            if (poweroftwo(newnum))
+            {
+                Console.WriteLine("It is a power of two");
+            }
+            else
+            {
+                Console.WriteLine("It is not a power of two");
+            }
+
+        }
+
+        bool poweroftwo(int n)
+        {
+            if( n== 0)
+            {
+                return false;
+            }
+            while(n!=1)
+            {
+                if (n % 2 != 0)
+                {
+                    return false;
+                }
+                n /= 2;
+            }
+            return true;
+        }
+    }
+}
---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = true;
+            while (t)
+            {
+                Console.WriteLine("1.Swap nibbles \n2.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Binary b = new Binary();
+                        Console.WriteLine("Enter the number");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        b.toBinary(num);
+                        break;
+                    case 2:
+                        t = false;
+                        break;
+                }
+            }
+        }
+    }
+}
