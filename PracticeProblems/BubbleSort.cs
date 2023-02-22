80f49d185441ec7f67773eb1b92a2dbf7e9d26b3
uditnm <udit20001@gmail.com>
2023-02-22 14:39:50 +0530

    [Udit] added program for Generic bubble sort

---
+++ PracticeProblems/BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class BubbleSort
+    {
+        public void sort<T>(T[] arr) where T:IComparable
+        {
+            int n = arr.Length;
+            for(int i = 0; i < n-1; i++)
+            {
+                for(int j = 0; j < n - 1 - i; j++)
+                {
+                    if (arr[j].CompareTo(arr[j+1])>0)
+                    {
+                        T temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+
+            for(int i=0;i<n;i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+        
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -9,7 +9,7 @@ namespace PracticeProblems
             bool t = true;
             while (t)
             {
-                Console.WriteLine("1.Swap nibbles \n2.Exit");
+                Console.WriteLine("1.Swap nibbles \n2.Generic BubbleSort \n3.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -20,6 +20,43 @@ namespace PracticeProblems
                         b.toBinary(num);
                         break;
                     case 2:
+                        BubbleSort bs = new BubbleSort();
+                        Console.WriteLine("Enter the no. of elements");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the values:");
+                        Console.WriteLine("1.int array \n2.string array \n3.float array");
+                        int ch = Convert.ToInt32(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case 1:
+                                int[] arr = new int[n];
+                                for (int i = 0; i < n; i++)
+                                {
+                                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                                }
+                                bs.sort(arr);
+                                break;
+                            case 2:
+                                string[] ar = new string[n];
+                                for (int i = 0; i < n; i++)
+                                {
+                                    ar[i] = Console.ReadLine();
+                                }
+                                bs.sort(ar);
+                                break;
+                            case 3:
+                                float[] a = new float[n];
+                                for (int i = 0; i < n; i++)
+                                {
+                                    a[i] = Convert.ToSingle(Console.ReadLine());
+                                }
+                                bs.sort(a);
+                                break;
+                        }
+                        
+                        
+                        break;
+                    case 3:
                         t = false;
                         break;
                 }
