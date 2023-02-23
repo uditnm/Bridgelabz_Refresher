5babb6d094c120af4adad1e6e6cbcf757d1dd780
uditnm <udit20001@gmail.com>
2023-02-23 23:15:13 +0530

    [Udit] added program for Split Words

---
+++ PracticeProblems/SplitWords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class SplitWords
+    {
+        public void split()
+        {
+            string path = @"D:\bridgelabz\Bridgelabz_Refresher\PracticeProblems\";
+            List<string> words = Regex.Split(File.ReadAllText(path + "file.txt"), @"[\s,;:.!?-]+").ToList();
+            Console.WriteLine("Enter the word: ");
+            string word = Console.ReadLine();
+            if (words.Contains(word))
+            {
+                words.Remove(word);
+            }
+            else
+            {
+                words.Add(word);
+            }
+            StreamWriter sw = new StreamWriter(path + "op.txt");
+            foreach (string detail in words)
+            {
+                sw.WriteLine(detail);
+            }
+            sw.Close();
+
+            Console.WriteLine("The list in output file is:");
+            string[] lines = File.ReadAllLines(path + "op.txt");
+
+            foreach (string line in lines)
+                Console.WriteLine(line);
+        }
+    }
+}
