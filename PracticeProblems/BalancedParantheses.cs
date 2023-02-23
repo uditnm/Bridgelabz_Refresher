935537fc7fa637b9b0e08c3c3a43a56aef70ae82
uditnm <udit20001@gmail.com>
2023-02-23 23:14:49 +0530

    [Udit] added program for balanced parantheses

---
+++ PracticeProblems/BalancedParantheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class BalancedParantheses
+    {
+        public bool balance(string check)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < check.Length; i++)
+            {
+                char character = check[i];
+                if (character == '[' || character == '{' || character == '(')
+                    stack.Push(character);
+                else if (character == ']' || character == '}' || character == ')')
+                {
+                    if (!stack.Any()) 
+                        return false;
+                    switch (character)
+                    {
+                        case ']':
+                            if (stack.Pop() != '[')
+                                return false;
+                            break;
+                        case '}':
+                            if (stack.Pop() != '{')
+                                return false;
+                            break;
+                        case ')':
+                            if (stack.Pop() != '(')
+                                return false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            
+            if (!stack.Any())
+                return true;
+            return false;
+        }
+    }
+}
