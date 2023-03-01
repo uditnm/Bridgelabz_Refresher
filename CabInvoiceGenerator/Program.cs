f71e69a5a2f306cf844e650aa5035aa212c1b623
uditnm <udit20001@gmail.com>
2023-03-01 20:29:37 +0530

    [Udit] added program for Cab Invoice Generator.

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = true;
+            while(t)
+            {
+                Console.WriteLine("1.Enter ride \n2.Exit");
+                int ch = Convert.ToInt32(Console.ReadLine());
+                switch(ch)
+                {
+                    case 1:
+                        
+                        CabInvoice c = new CabInvoice();
+                        c.CalculateFare();
+                        break;
+                    case 2:
+                        t = false;
+                        break;
+                }
+            }
+            
+        }
+    }
+}
